bc1f153e8e73611d4cbd97c1a6614dac2ea57c50
Hikaru Ojima <ojima.h@gmail.com>
2016-12-02 21:15:00 +0900

    Add abides revoke command

---
+++ lib/abid/cli/revoke.rb
@@ -0,0 +1,46 @@
+require 'abid/cli/table_formatter'
+
+module Abid
+  class CLI
+    class Revoke
+      def initialize(options, job_ids)
+        @options = options
+        @job_ids = job_ids.map(&:to_i)
+
+        @force = @options[:force]
+        @quiet = @options[:quiet]
+      end
+
+      def run
+        @job_ids.each do |job_id|
+          state = StateManager::State[job_id]
+          if state.nil?
+            $stderr.puts "state_id #{job_id} not found"
+            next
+          end
+
+          next if !@quiet && !ask(state)
+
+          revoke(state)
+        end
+      end
+
+      def revoke(state)
+        state.revoke(force: @force)
+        puts "revoked #{state.id}"
+      rescue AlreadyRunningError
+        params = ParamsFormat.format(YAML.load(state.params))
+        $stderr.puts "#{state.name} #{params} already running.\n" \
+                     'Use -f option if you want to force assume.'
+      end
+
+      def ask(state)
+        params = ParamsFormat.format(YAML.load(state.params))
+        print "revoke task \`#{state.name} #{params}'? "
+        $stdout.flush
+        ret = $stdin.gets
+        ret.match(/y(es)?/i)
+      end
+    end
+  end
+end
