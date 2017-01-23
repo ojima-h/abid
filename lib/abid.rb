f60b2fc7033e39b1eb558fe2627a6559a5854510
Hikaru Ojima <ojima.h@gmail.com>
2017-01-24 02:50:50 +0900

    Remove ParamsParser

--- lib/abid.rb
+++ lib/abid.rb
@@ -11,21 +11,17 @@ require 'rbtree'
 require 'sqlite3'
 require 'sequel'
 
+require 'abid/application'
 require 'abid/config'
 require 'abid/environment'
 require 'abid/error'
 require 'abid/engine'
 require 'abid/params_format'
-require 'abid/state_manager'
-
 require 'abid/rake_extensions'
+require 'abid/state_manager'
 require 'abid/version'
-require 'abid/params_parser'
-require 'abid/application'
 
 module Abid
-  FIXNUM_MAX = (2**(0.size * 8 - 2) - 1) # :nodoc:
-
   class << self
     def global
       @global ||= Environment.new
