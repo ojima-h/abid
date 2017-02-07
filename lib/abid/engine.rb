e56dbdd9abe0fb97f2b99dc1894b4b807acf8523
Hikaru Ojima <ojima.h@gmail.com>
2017-02-07 09:48:32 +0900

    Add engine.rb

--- lib/abid/engine.rb
+++ lib/abid/engine.rb
@@ -21,10 +21,15 @@ module Abid
     attr_reader :job_manager, :worker_manager
     alias jobs job_manager
     def_delegators :@env, :options, :state_manager, :logger
-    def_delegators :job_manager, :summary, :pretty_summary, :errors
+    def_delegators :job_manager, :summary, :errors
 
-    end
-
+    # @param name [String] task name
+    # @param params [Hash]
+    # @param args [Array]
+    # @return [(Symbol, Exception)] pair of result and error.
+    def invoke(name, *args, **params)
+      task = @env.application.abid_tasks[name, params]
+      jobs.invoke(task, args)
     end
 
     def kill(error)
@@ -32,6 +37,9 @@ module Abid
       job_manager.kill(error)
     end
 
+    def shutdown
+      job_manager.shutdown
+      worker_manager.shutdown
     end
   end
 end
